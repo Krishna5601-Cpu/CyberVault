2e0cc169daa9f46daed023c770941a74bc565e24
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-09-25 21:55:06 +0530

    feat: add ANSI color utility

--- utils/Colors.h
+++ utils/Colors.h
@@ -1,12 +1,25 @@
 #pragma once
 
+// Reset
 #define RESET "\033[0m"
 
+// Regular Colors
+#define BLACK "\033[30m"
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 #define BLUE "\033[34m"
 #define MAGENTA "\033[35m"
 #define CYAN "\033[36m"
+#define WHITE "\033[37m"
 
+// Bright Colors
+#define BRIGHT_RED "\033[91m"
+#define BRIGHT_GREEN "\033[92m"
+#define BRIGHT_YELLOW "\033[93m"
+#define BRIGHT_BLUE "\033[94m"
+#define BRIGHT_MAGENTA "\033[95m"
+#define BRIGHT_CYAN "\033[96m"
+
+// Style
 #define BOLD "\033[1m"
